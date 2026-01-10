b335511a4fec915e494790cc2ee15cb2321cf9db
katsu-0709-admin <tea0cl@gmail.com>
2026-01-10 22:14:15 +0900

    ユーザーの新規登録・ログイン処理の実装、新規登録処理にバリデーション処理の実装完了！

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,6 +30,21 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+
+        $request->validate([
+            // ユーザー名：入力必須、2文字以上、12文字以内
+            'username' => ['required', 'string', 'min:2', 'max:12'],
+
+            // メールアドレス：入力必須、5文字以上、40文字以内、重複不可、メール形式
+            'email' => ['required', 'string', 'email', 'min:5', 'max:40', 'unique:users,email'],
+
+            // パスワード：入力必須、英数字のみ、8文字以上、20文字以内、確認欄と一致
+            'password' => ['required', 'string', 'alpha_num', 'min:8', 'max:20', 'confirmed'],
+
+            // パスワード確認：入力必須、英数字のみ、8文字以上、20文字以内
+            'password_confirmation' => ['required', 'string', 'alpha_num', 'min:8', 'max:20'],
+        ]);
+
         User::create([
             'username' => $request->username,
             'email' => $request->email,
